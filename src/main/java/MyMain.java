a03e70c8fed8da79af276cfbc7d317d23f54c50c
23eliu <23eliu@athenian.org>
2020-09-05 00:58:30 +0000

    wrote main methods

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,18 +4,27 @@ public class MyMain {
 
     // Calculates the median of the three inputs
     public static int median(int a, int b, int c) {
-        return 0; // REPLACE WITH YOUR CODE
+        int big1 = Math.max(a, b);
+        if (big1 < c) {
+            return big1;
+        }
+        return Math.max(Math.min(a, b), c);
+
     }
 
     // Returns the input with the larger absolute value
     public static int magnitude(int a, int b) {
-        return 0; // REPLACE WITH YOUR CODE
+        if (Math.abs(a) > Math.abs(b)) {
+            return a;
+        }
+        return b;
     }
 
     // Returns the "c" value from the Pythagorean theorem "a^2 + b^2 = c^2",
     // where "a" and "b" are the inputs to the method
     public static double pythagoras(int a, int b) {
-        return 0.0; // REPLACE WITH YOUR CODE
+        double squared = (Math.pow(a, 2) + Math.pow(b, 2));
+        return Math.sqrt(squared);
     }
 
     public static void main(String[] args) {
@@ -25,6 +34,6 @@ public class MyMain {
         System.out.println(pythagoras(3, 4)); // should be 5.0
 
         Scanner scan = new Scanner(System.in);
-        // YOUR CODE HERE
+        
     }
 }
